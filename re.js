355ffb70aa025af124150bc4521795bb07f0e2aa
cyfarren <67444385+cyfarren@users.noreply.github.com>
2022-04-11 21:39:27 -0400

    Create re.js

---
+++ re.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'ex8/style.css';
+import App from 'ex9/qApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
